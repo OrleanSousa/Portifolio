2dfabc5fba030f596b1b2569dde7a1e98999c1a0
Orlean Sousa <99688852+OrleanSousa@users.noreply.github.com>
2025-02-14 14:48:00 -0300

    ajustando foto principal

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"], // Ordem corrigida, mas igual à sua
   theme: {
     screens:{
-      ssm: '375px',
+      ssm: '320px',
       sm: '640px',
       md: '768px',
       lg: '1024px',
